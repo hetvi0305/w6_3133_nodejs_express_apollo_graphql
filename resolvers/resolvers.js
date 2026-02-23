b6bcd2d797f5f7c5e5faa090b4bfdb7cbf5eefea
Hetvi Patel <hetvi.patel3@georgebrown.ca>
2026-02-22 23:14:38 -0500

    Lab05 complete

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,3 +1,56 @@
-import MovieModel from '../models/movie.js';
+import MovieModel from "../models/movie.js";
 
-// Resolvers define the technique for fetching the types defined in the schema.
+const movieResolvers = {
+    Query: {
+        // a) Get all movies
+        getAllMovies: async () => {
+        return await MovieModel.find();
+        },
+
+        // b) Get movie by ID
+        getMovieById: async (_, { id }) => {
+        return await MovieModel.findById(id);
+        },
+
+        // c) Get movies by Director name using static methods
+        getMoviesByDirector: async (_, { director_name }) => {
+        return await MovieModel.findByDirector(director_name);
+        },
+    },
+
+    Mutation: {
+        // a) Insert new movie
+        insertMovie: async (_, { input }) => {
+        const movie = new MovieModel(input);
+        return await movie.save();
+        },
+
+        // b) Update movie
+        updateMovie: async (_, { id, input }) => {
+        const updatedMovie = await MovieModel.findByIdAndUpdate(
+            id,
+            { $set: input },
+            { new: true, runValidators: true }
+        );
+
+        if (!updatedMovie) {
+            throw new Error("Movie not found");
+        }
+
+        return updatedMovie;
+        },
+
+        // c) Delete movie by ID
+        deleteMovieById: async (_, { id }) => {
+        const deleted = await MovieModel.findByIdAndDelete(id);
+        return !!deleted;
+        },
+    },
+
+    // Map MongoDB _id -> GraphQL id
+    Movie: {
+        id: (parent) => parent._id.toString(),
+    },
+};
+
+export default movieResolvers;
--- schemas/schema.js
+++ schemas/schema.js
@@ -1,1 +1,52 @@
-import { gql } from 'graphql-tag';
+import { gql } from "graphql-tag";
+
+const movieSchema = gql`
+    type Movie {
+        id: ID!
+        name: String!
+        director_name: String!
+        production_house: String!
+        release_date: String!
+        rating: Float!
+    }
+
+    type Query {
+        # a) Get all movies
+        getAllMovies: [Movie!]!
+
+        # b) Get movie by ID
+        getMovieById(id: ID!): Movie
+
+        # c) Get movies by Director name (uses static method in resolver)
+        getMoviesByDirector(director_name: String!): [Movie!]!
+    }
+
+    input MovieInput {
+        name: String!
+        director_name: String!
+        production_house: String!
+        release_date: String!
+        rating: Float!
+    }
+
+    input UpdateMovieInput {
+        name: String
+        director_name: String
+        production_house: String
+        release_date: String
+        rating: Float
+    }
+
+    type Mutation {
+        # a) Insert new movie
+        insertMovie(input: MovieInput!): Movie!
+
+        # b) Update movie
+        updateMovie(id: ID!, input: UpdateMovieInput!): Movie!
+
+        # c) Delete movie by ID
+        deleteMovieById(id: ID!): Boolean!
+    }
+`;
+
+export default movieSchema;
